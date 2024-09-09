7c56e4dc9932bd39e49d2214f4b5a43cf295b673
Chayakarn Hengsuwan <chayakarn.h@ku.th>
2024-09-09 14:29:23 +0700

    fix test_statistics follows statistics

--- test_statistics.py
+++ test_statistics.py
@@ -1,23 +1,27 @@
+"""Unit tests for the statistics module."""
 from unittest import TestCase
 from statistics import variance, stdev
 from math import sqrt
 
+
 class StatisticsTest(TestCase):
+    """Class for testing statistics functions."""
 
     def test_variance_typical_values(self):
-        """variance of typical values"""
-        self.assertEqual(0.0, variance([10.0,10.0,10.0,10.0,10.0]))
-        self.assertEqual(2.0, variance([1,2,3,4,5]))
-        self.assertEqual(8.0, variance([10,2,8,4,6]))
-    
+        """Variance of typical values."""
+        self.assertEqual(0.0, variance([10.0, 10.0, 10.0, 10.0, 10.0]))
+        self.assertEqual(2.0, variance([1, 2, 3, 4, 5]))
+        self.assertEqual(8.0, variance([10, 2, 8, 4, 6]))
+
     def test_variance_non_integers(self):
-        """variance should work with decimal values"""
+        """Variance should work with decimal values."""
         # variance([x,y,z]) == variance([x+d,y+d,z+d]) for any d
         self.assertAlmostEqual(4.0, variance([0.1, 4.1]))
         # variance([0,4,4,8]) == 8
         self.assertAlmostEqual(8.0, variance([0.1, 4.1, 4.1, 8.1]))
 
     def test_stdev(self):
+        """`stdev` should return the square root of the variance."""
         # standard deviation of a single value should be zero
         self.assertEqual(0.0, stdev([10.0]))
         # simple test
@@ -28,5 +32,5 @@ class StatisticsTest(TestCase):
 
 if __name__ == '__main__':
     import unittest
-    unittest.main(verbosity=1)
 
+    unittest.main(verbosity=1)
